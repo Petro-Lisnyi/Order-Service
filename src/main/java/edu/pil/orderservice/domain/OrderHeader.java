e81bbb5085e87376898ea649256a911c0dfaccac
Petro Lisnyi <petro.lisnyi@gmail.com>
2024-02-10 21:56:04 +0200

    Add BaseEntity abstract class

--- src/main/java/edu/pil/orderservice/domain/OrderHeader.java
+++ src/main/java/edu/pil/orderservice/domain/OrderHeader.java
@@ -2,13 +2,28 @@ package edu.pil.orderservice.domain;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
 @Entity
-public class OrderHeader {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class OrderHeader extends BaseEntity{
     private String customer;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderHeader that)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(getCustomer(), that.getCustomer());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getCustomer());
+    }
 }
