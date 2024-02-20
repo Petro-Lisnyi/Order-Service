884df744ace081496e449125659a5dcf4aed2627
Petro Lisnyi <petro.lisnyi@gmail.com>
2024-02-20 15:36:03 +0200

    Add the Bootstrap class for testing db transactions in Spring context

---
+++ src/main/java/edu/pil/orderservice/bootstrap/Boootstrap.java
@@ -0,0 +1,10 @@
+package edu.pil.orderservice.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Boootstrap implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
